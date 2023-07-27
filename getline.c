8e4a2fac9ee02c2a8d9b75ffe33be9f548e0f81c
SisterToch <ibekwetestimony3@gmail.com>
2023-07-27 09:29:34 -0700

    getline

--- getline.c
+++ getline.c
@@ -1,23 +1,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "main.h"
+/**
+ * _getline - Reads the input
+ * @lineptr: Pointer to the buffer
+ * @n: Pointer to the size of the buffer
+ * Return: number of char read
+ */
 
 ssize_t _getline(char **lineptr, size_t *n)
 {
-	/*if (lineptr == NULL || n == NULL)
-	{
-		write(STDERR_FILENO, "Error: Invalid input\n", 21);
-		 return (-1);
-	}*/
-
 	size_t position = 0;/*in the buffer*/
 	char store;/*stores the character to be read*/
 
-	if (*lineptr == NULL)
-	{/*allocate memory
-		*n = 128;/*random initial buffer size*/
+	if (*lineptr == NULL)/*allocate memory*/
+	{	*n = 128;/*random initial buffer size*/
 		*lineptr = (char *)malloc(*n);
-
 		if (*lineptr == NULL)
 		{
 			write(STDERR_FILENO, "Error:memory alloc failed", 30);
@@ -34,20 +32,27 @@ ssize_t _getline(char **lineptr, size_t *n)
 
 		if (position >= *n - 1)
 		{/*if the position is greater than the buffer, double it*/
-			size_t new_buffer = (*n) * 2;
-			char *new_lineptr = realloc(*lineptr, new_buffer);
-			
+			size_t new_buffer = *n + 128;
+			char *new_lineptr = (char *)malloc(new_buffer);
+
 			if (new_lineptr == NULL)
 			{
 				write(STDERR_FILENO, "Error: Memory alloc failed\n", 27);
-				return -1;
+				free(*lineptr);
+				*lineptr = NULL;
+				return (-1);
 			}
+			for (size_t i = 0; i < position; i++)
+			{
+				new_lineptr[i] = (*lineptr)[i];
+			}
+
+			free(*lineptr);
 			*lineptr = new_lineptr;
 			*n = new_buffer;
 		}
 
-		(*lineptr)[position] = store;
-		position++;
+		(*lineptr)[position++] = store;
 
 		if (store == '\n')
 		{
